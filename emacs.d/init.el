e50a85537d58ed21793990ca954d1c2027506957
Sondre Nilsen <nilsen.sondre@gmail.com>
2015-10-04 19:53:50 +0200

    El Capitan cleaning

--- emacs.d/init.el
+++
@@ -1,9 +0,0 @@
-(require 'cask "~/.cask/cask.el")
-(cask-initialize)
-(require 'pallet)
-(pallet-mode t)
-
-(require 'ob-tangle)
-(org-babel-load-file
- (expand-file-name "emacs-init.org"
-		               user-emacs-directory))
--- profiles.clj
+++
@@ -1,2 +0,0 @@
-{:user {:plugins [[cider/cider-nrepl "0.8.2-SNAPSHOT"]
-                  [lein-try "0.4.3"]]}}
