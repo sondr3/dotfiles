4745e50630c129f50bf06894aeaed6b537bf2399
Sondre Nilsen <nilsen.sondre@gmail.com>
2015-10-04 21:01:41 +0200

    Moar dots, moar dots

--- config.fish
+++ config.fish
@@ -1,14 +1,6 @@
 # Remove greeting
 set -g -x fish_greeting ''
 
-# Make Chruby work
-source /usr/local/share/chruby/chruby.fish
-source /usr/local/share/chruby/auto.fish
-chruby 2.2.0
-
-#direnv
-eval (direnv hook fish)
-
 # base16 shell
 eval sh $HOME/.config/base16-shell/base16-solarized.dark.sh
 
