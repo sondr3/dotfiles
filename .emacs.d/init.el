6356d2c3c083bd31adae47b817c4742d6b7dd809
Sondre Nilsen <nilsen.sondre@gmail.com>
2016-02-19 17:31:38 +0100

    More Emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,8 +2,23 @@
 (setq user-full-name "Sondre Nilsen"
       user-mail-addres "nilsen.sondre@gmail.com")
 
-(When window-system
+(when window-system
   (tooltip-mode -1)
   (tool-bar-mode -1)
   (menu-bar-mode 1)
   (scroll-bar-mode -1))
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/")
+             '("marmalade" . "https://marmalade-repo.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+
+(use-package auto-compile
+  :ensure t
+  :config (auto-compile-on-load-mode))
+(setq load-prefer-newer t)
