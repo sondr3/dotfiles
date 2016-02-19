0a3c1e81db6b78e0dfee9e89bb8f74cd92036759
Sondre Nilsen <nilsen.sondre@gmail.com>
2016-02-19 17:27:50 +0100

    Start on Emacs... again

---
+++ .emacs.d/init.el
@@ -0,0 +1,9 @@
+
+(setq user-full-name "Sondre Nilsen"
+      user-mail-addres "nilsen.sondre@gmail.com")
+
+(When window-system
+  (tooltip-mode -1)
+  (tool-bar-mode -1)
+  (menu-bar-mode 1)
+  (scroll-bar-mode -1))
