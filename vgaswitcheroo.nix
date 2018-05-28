f4844e12b0f279b06626d53960b00d209363e80a
Sondre Nilsen <nilsen.sondre@gmail.com>
2018-05-28 15:14:31 +0200

    Update vgaswitcheroo

--- vgaswitcheroo.nix
+++ vgaswitcheroo.nix
@@ -27,8 +27,8 @@ in {
       serviceConfig = {
         Type = "oneshot";
         RemainAfterExit = "yes";
-        ExecStart = "${pkgs.bash}/bin/sh -c 'echo OFF > /sys/kernel/debug/vgaswitcheroo/switch'";
-        ExecStop = "${pkgs.bash}/bin/sh -c 'echo ON >/sys/kernel/debug/vgaswitcheroo/switch'";
+        ExecStart = "${pkgs.bash}/bin/sh -c 'echo -e \"DIGD\\nOFF\" > /sys/kernel/debug/vgaswitcheroo/switch'";
+        ExecStop = "${pkgs.bash}/bin/sh -c 'echo ON > /sys/kernel/debug/vgaswitcheroo/switch'";
       };
     };
     systemd.paths."vgaswitcheroo" = {
