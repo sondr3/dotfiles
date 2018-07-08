403112fa062f8494f3d61dcaef88faa436aeeab8
Sondre Nilsen <nilsen.sondre@gmail.com>
2018-07-08 15:53:33 +0200

    Assimilate diff-hl

--- base/base-git.el
+++ base/base-git.el
@@ -17,10 +17,17 @@
 
 ;;; Commentary:
 
-;; commentary
+;; Like pretty much everybody nowadays I'm using ~git~, and with that comes
+;; probably one of the absolutely best packages that exists for Emacs: Magit!
+;; Even if you're okay at using git from the command line, Magit just blows the
+;; command line interface for git out of the water. If you haven't tried it I
+;; highly recommend it.
 
 ;;; Code:
 
+;;; `Magit':
+;; Enable and appreciate it! The only thing we'll really change is adding a few
+;; extra functions and hooks to work better with Borg.
 (use-package magit
   :delight auto-revert-mode
   :general
@@ -35,10 +42,36 @@
                             'magit-insert-stashes
                             'append)))
 
+;;; `evil-magit':
+;; Magit by default doesn't include any Evil keybindings, which makes sense but
+;; is kinda required since we use Evil.
 (use-package evil-magit
   :after magit
   :commands evil-magit-init
   :init (evil-magit-init))
+
+;;; `diff-hl':
+;; There's a plugin for Vim called GitGutter that is really neat, in the fringe
+;; of your file it shows where hunks have been changed, added and removed from
+;; the file. There's a similarly named plugin for Emacs, but it hasn't been
+;; updated for quite a while and even then, `diff-hl' is quite a lot better than
+;; it is. There's no magic here, we'll enable it globally, hook into Magit so
+;; that diff-hl updates when we commit using Magit.
+(use-package diff-hl
+  :commands (diff-hl-magit-post-refresh global-diff-hl-mode)
+  :functions (diff-hl-flydiff-mode diff-hl-margin-mode)
+  :defines diff-hl-margin-symbols-alist
+  :init
+  (progn
+    (setq diff-hl-margin-symbols-alist
+          '((insert . "+") (delete . "-") (change . "~")
+            (unknown . "?") (ignored . "i"))))
+  :config
+  (progn
+    (global-diff-hl-mode)
+    (diff-hl-flydiff-mode)
+    (diff-hl-margin-mode)
+    (add-hook 'magit-post-refresh-hook #'diff-hl-magit-post-refresh t)))
 
 (provide 'base-git)
 
