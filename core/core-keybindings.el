9783d3888b450ade0808ad5081768621e47a2aa5
Sondre Nilsen <nilsen.sondre@gmail.com>
2018-07-27 20:07:26 +0200

    Sort which-key menu different

--- core/core-keybindings.el
+++ core/core-keybindings.el
@@ -51,6 +51,8 @@
   :config
   (progn
     (setq-am which-key-idle-delay 0.3 "Reduce the time before which-key pops up")
+    (setq which-key-allow-evil-operators t
+          which-key-sort-order 'which-key-key-order-alpha)
     (which-key-mode)))
 
 ;;; `General':
