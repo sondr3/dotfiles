2b3f9ef17b77166fe6bbc5255992d2f5876f2a52
Sondre Nilsen <nilsen.sondre@gmail.com>
2018-11-13 15:14:28 +0100

    Don't put captions above _anything_

--- modules/org/org-latex-export.el
+++ modules/org/org-latex-export.el
@@ -119,6 +119,7 @@
           org-latex-default-class "memoir"                   ;; Use my own class by default
           org-latex-default-table-environment "tabularx"     ;; Use a better table formatter
           org-latex-tables-booktabs t                        ;; Always use booktabs for better looking tables
+          org-latex-caption-above nil                        ;; Put captions below everything
           org-latex-listings t                               ;; Make SRC blocks export to code blocks in LaTeX
           org-latex-pdf-process (list "latexmk -pvc- %f")    ;; Use `latexmk' to generate PDF
           org-latex-listings-options                         ;; Configure source code exporting
