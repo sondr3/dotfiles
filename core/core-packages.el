101aa9ff00a77efc6a0b17aef1c6c08bac42be7d
Sondre Nilsen <nilsen.sondre@gmail.com>
2018-11-14 13:06:14 +0100

    epkg doesn't require it's own directory, just use the cache/ directory

--- core/core-packages.el
+++ core/core-packages.el
@@ -49,7 +49,7 @@
 (setq use-package-always-defer t)                    ;; Always defer packages
 
 (use-package epkg
-  :init (setq epkg-repository (expand-file-name "epkgs/" amalthea-cache-dir))) ;; Save package database in our cache directory
+  :init (setq epkg-repository amalthea-cache-dir)) ;; Save package database in our cache directory
 
 (use-package delight
   :demand t)
