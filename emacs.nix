19891b87a5f0eaafeb565f8b3606c2e8cab07623
Sondre Nilsen <nilsen.sondre@gmail.com>
2018-11-19 22:54:04 +0100

    Start building Emacs and packages with Nix

--- emacs.nix
+++ emacs.nix
@@ -1,23 +1,80 @@
-{ pkgs, ...}:
+{ pkgs ? import <nixpkgs> {} }:
 
 let
-  emacs26 = pkgs.stdenv.lib.overrideDerivation pkgs.emacs (oldAttrs : {
-    version = "26.1RC1";
-    src = pkgs.fetchurl {
-      url = ftp://alpha.gnu.org/gnu/emacs/pretest/emacs-26.1-rc1.tar.xz;
-      sha256 = "6594e668de00b96e73ad4f168c897fe4bca7c55a4caf19ee20eac54b62a05758";
-    };
-    withGTK3 = true;
-    withGTK2 = false;
-    patches = [];
-  });
-  emacs = emacs26;
+  amalthea = pkgs.emacs;
+  emacsWithPackages = (pkgs.emacsPackagesNgGen amalthea).emacsWithPackages;
 in
-{
-  environment.systemPackages = [
-      emacs
-  ];
+  emacsWithPackages (epkgs: (with epkgs.elpaPackages; [
+    delight
+    async
+    undo-tree
+    aggressive-indent
+    auctex
+    hydra
+  ]) ++ (with epkgs.melpaPackages; [
+    use-package
+    which-key
+    general
+    exec-path-from-shell
+    projectile
+    counsel-projectile
+    evil
+    evil-collection
+    evil-lion
+    evil-goggles
+    evil-surround
+    evil-commentary
+    evil-smartparens
+    evil-magit
+    magit
+    diff-hl
+    hl-todo
+    helpful
+    rainbow-delimiters
+    smartparens
+    ws-butler
+    ivy
+    counsel
+    swiper
+    amx
+    company
+    company-quickhelp
+    company-statistics
+    yasnippet
+    yasnippet-snippets
+    ivy-yasnippet
+    deadgrep
+    flyspell-correct-ivy
+    flycheck
+    apropospriate-theme
+    org-ref
+    nginx-mode
+    company-nginx
+    yaml-mode
+    dockerfile-mode
+    nasm-mode
+    auto-compile
+    macrostep
+    rust-mode
+    json-mode
+    markdown-mode
+    auctex-latexmk
+    company-auctex
+    company-math
+    magic-latex-buffer
+    latex-extra
+    company-reftex
+    ivy-bibtex
+    ebib
+    company-shell
+    haskell-mode
+    intero
+    flycheck-haskell
+    hlint-refactor
+    nix-mode
+  ]) ++ (with epkgs.orgPackages; [
+    org
+    org-plus-contrib
+  ]) ++ [
 
-  services.emacs.package = emacs;
-  services.emacs.enable = true;
-}
+  ])
