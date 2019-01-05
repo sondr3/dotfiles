9da54611e9768733fd029ad9bd34405191e373b1
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-01-05 15:02:44 +0100

    Add outshine package, sort packages alphabetically

--- emacs.nix
+++ emacs.nix
@@ -5,77 +5,78 @@ let
   emacsWithPackages = (pkgs.emacsPackagesNgGen amalthea).emacsWithPackages;
 in
   emacsWithPackages (epkgs: (with epkgs.elpaPackages; [
-    delight
-    undo-tree
     aggressive-indent
     auctex
+    delight
+    undo-tree
   ]) ++ (with epkgs.melpaStablePackages; [
-    magit
-    company
-    no-littering
     auto-compile
-    hl-todo
-    markdown-mode
-    hydra
-    yasnippet
-    projectile
-    macrostep
+    company
     gitattributes-mode
     gitconfig-mode
     gitignore-mode
+    hl-todo
+    hydra
+    macrostep
+    magit
+    markdown-mode
+    no-littering
+    projectile
+    yasnippet
   ]) ++ (with epkgs.melpaPackages; [
-    use-package
-    which-key
-    general
-    exec-path-from-shell
-    counsel-projectile
-    evil
-    evil-collection
-    evil-lion
-    evil-goggles
-    evil-surround
-    evil-commentary
-    evil-smartparens
-    evil-magit
-    diff-hl
-    helpful
-    rainbow-delimiters
-    smartparens
-    ws-butler
-    ivy
-    counsel
-    swiper
     amx
-    company-quickhelp
-    company-statistics
-    yasnippet-snippets
-    ivy-yasnippet
-    deadgrep
-    flyspell-correct-ivy
-    flycheck
     apropospriate-theme
-    org-ref
-    nginx-mode
-    company-nginx
-    yaml-mode
-    dockerfile-mode
-    nasm-mode
-    rust-mode
-    json-mode
     auctex-latexmk
     company-auctex
     company-math
-    magic-latex-buffer
-    latex-extra
+    company-nginx
+    company-quickhelp
     company-reftex
-    ivy-bibtex
-    ebib
     company-shell
-    haskell-mode
-    intero
+    company-statistics
+    counsel
+    counsel-projectile
+    deadgrep
+    diff-hl
+    dockerfile-mode
+    ebib
+    evil
+    evil-collection
+    evil-commentary
+    evil-goggles
+    evil-lion
+    evil-magit
+    evil-smartparens
+    evil-surround
+    exec-path-from-shell
+    flycheck
     flycheck-haskell
+    flyspell-correct-ivy
+    general
+    haskell-mode
+    helpful
     hlint-refactor
+    intero
+    ivy
+    ivy-bibtex
+    ivy-yasnippet
+    json-mode
+    latex-extra
+    magic-latex-buffer
+    nasm-mode
+    nginx-mode
     nix-mode
+    org-ref
+    outshine
+    rainbow-delimiters
+    rust-mode
+    smartparens
+    swiper
+    use-package
+    which-key
+    ws-butler
+    yaml-mode
+    yasnippet-snippets
   ]) ++ (with epkgs.orgPackages; [
     org
     org-plus-contrib
