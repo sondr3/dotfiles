c923d582cca982fa05a75a7d913b0e7fd41fcd74
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-02-06 13:35:35 +0100

    Add configuration for neptune

---
+++ neptune.nix
@@ -0,0 +1,132 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.kernelPackages = pkgs.linuxPackages_4_19;
+  boot.kernelModules = [ "amdgpu.dc=1" ];
+  boot.cleanTmpDir = true;
+  boot.plymouth.enable = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  hardware.cpu.amd.updateMicrocode = true;
+
+  networking.hostName = "neptune"; # Define your hostname.
+  networking.networkmanager.enable = true;
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  fonts = {
+    enableFontDir = true;
+    enableGhostscriptFonts = true;
+    fontconfig.dpi = 221;
+    fonts = with pkgs; [
+      corefonts
+      inconsolata
+      fira
+      fira-mono
+      dejavu_fonts
+      libertine
+      ubuntu_font_family
+      noto-fonts
+      source-code-pro
+    ];
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Oslo";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    # core tools
+    coreutils gitAndTools.gitFull htop
+    wget curl zip unzip tree ranger
+    acpi psmisc pciutils bind
+
+    # editing
+    neovim
+
+    # useful
+    gnupg
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.mtr.enable = true;
+  programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+  programs.fish.enable = true;
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.support32Bit = true;
+
+  # OpenGL
+  hardware.opengl.driSupport32Bit = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.videoDrivers = [ "amdgpu" ];
+  services.xserver.layout = "us";
+  services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable touchpad support.
+  # services.xserver.libinput.enable = true;
+
+  # Enable the KDE Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  nixpkgs.config = {
+    allowUnfree = true;
+  };  
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.sondre = {
+    isNormalUser = true;
+    description = "Sondre Nilsen";
+    extraGroups = [ "wheel" "networkmanager" ];
+    shell = pkgs.fish;
+  };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "18.09"; # Did you read the comment?
+
+}
