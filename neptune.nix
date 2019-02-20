98fe2bb638d76e5fcad846196e7eaece65a831c5
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-02-20 20:46:55 +0100

    Open up port 8000 through firewall

--- neptune.nix
+++ neptune.nix
@@ -85,8 +85,8 @@
   services.openssh.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
+  networking.firewall.allowedTCPPorts = [ 8000 ];
+  networking.firewall.allowedUDPPorts = [ ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
