19fd406073abee71520f9fb9b12a96f5c341c853
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-07-22 21:41:10 +0200

    Remove yarn from my configuration, begone

--- configuration/home/programming.nix
+++ configuration/home/programming.nix
@@ -1,16 +1,11 @@
 { pkgs, ... }:
 
-let
-  # Link Yarn against latest Node instead of stable
-  yarn = pkgs.yarn.override { nodejs = pkgs.nodejs-11_x; };
-in
 {
   home.packages = with pkgs; [
     # Go
     go
 
     # Node
-    yarn
     nodejs-11_x
 
     # Python
