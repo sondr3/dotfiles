7c6429b18b62d61637c7aba83c6578d468b37109
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-10-14 16:02:13 +0200

    Move Discord into social
    Sadly too many communities rely on using it for their social platform instead of
    actual good platforms like Matrix or Zulip. Hell, even IRC would be an upgrade
    over the draconian TOS that Discord has.

--- home/gaming.nix
+++ home/gaming.nix
@@ -2,7 +2,6 @@
 
 {
   home.packages = with pkgs; [
-    discord
     minecraft
     steam
     teamspeak_client
--- home/social.nix
+++ home/social.nix
@@ -2,6 +2,7 @@
 
 {
   home.packages = with pkgs; [
+    discord
     slack
     tdesktop
     weechat
