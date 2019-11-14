b5c25285e61de3b6e855e9da484b8158508cbc5d
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-11-14 21:11:08 +0100

    Add and configure neovim with vim-plug

--- home/neovim.nix
+++ home/neovim.nix
@@ -1,12 +1,29 @@
 { pkgs, ... }:
 
 let
-  vimPlugins = pkgs.vimPlugins;
+  unstable = import <unstable> {};
 in
 {
   programs.neovim = {
     enable = true;
+    package = unstable.neovim-unwrapped;
     viAlias = true;
     vimAlias = true;
+    withNodeJs = true;
+    withPython = false;
+    withPython3 = true;
+
+    extraConfig = ''
+      call plug#begin(expand('~/.config/nvim/plugged'))
+
+      call plug#end()
+    '';
   };
+
+  xdg.dataFile."nvim/site/autoload/plug.vim".source = pkgs.fetchFromGitHub {
+    owner = "junegunn";
+    repo = "vim-plug";
+    rev = "68fef9c2fd9d4a21b500cc2249b6711a71c6fb9f";
+    sha256 = "0azmnxq82frs375k5b9yjdvsjfmzjv92ifqnmniar19d96yh6swa";
+  } + "/plug.vim";
 }
