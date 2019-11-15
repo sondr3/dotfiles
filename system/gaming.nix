dc5403783dc3adba77a47a72b39a3ea1f7083c61
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-11-15 23:58:34 +0100

    Fix Steam missing a library

--- system/gaming.nix
+++ system/gaming.nix
@@ -1,7 +1,10 @@
+{ pkgs, ... }:
+
 {
   # OpenGL support for Steam etc
   hardware = {
     opengl.driSupport32Bit = true;
+    opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
     pulseaudio.support32Bit = true;
   };
 }
