88c87362f1f80cd6d3aceda05cc952db605c006c
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-11-17 03:29:04 +0100

    Move `hardware` to root config, merge gaming configuration

--- config/gaming.nix
+++ config/gaming.nix
@@ -1,10 +1,23 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
+with lib;
+
+let
+  cfg = options.mine.gaming;
+in
 {
-  # OpenGL support for Steam etc
-  hardware = {
-    opengl.driSupport32Bit = true;
-    opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
-    pulseaudio.support32Bit = true;
+  options.mine.gaming.enable = mkEnableOption "Gaming";
+
+  config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      minecraft
+      steam
+    ];
+    # OpenGL support for Steam etc
+    hardware = {
+      opengl.driSupport32Bit = true;
+      opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
+      pulseaudio.support32Bit = true;
+    };
   };
 }
