b6ee04fcfd50daecee421494c1ce1f29f4d8e1a1
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-12-10 00:36:26 +0100

    Convert serve to abbrevation, doesn't work as alias

--- config/home/fish/default.nix
+++ config/home/fish/default.nix
@@ -13,15 +13,13 @@ with import ../../../lib;
     interactiveShellInit = ''
       source (jump shell fish | psub)
     '';
-    shellAliases = {
-      serve = "nix run nixpkgs.caddy -c caddy -host 0.0.0.0 -port 8000 browse";
-    };
     shellAbbrs = {
       pbcopy = "xclip -selection clipboard";
       pbpaste = "xclip -o -selection clipboard";
       nrs = "sudo nixos-rebuild switch";
       nrt = "sudo nixos-rebuild test";
       nrn = "nix repl \"<nixpkgs>\"";
+      serve = "nix run nixpkgs.caddy -c caddy -host 0.0.0.0 -port 8000 browse";
     };
   };
 
