885fc3c77bc2da1ad2f2f39e472154832f81bb05
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-02-07 23:21:02 +0100

    Don't use recImport, it tries to import shell.nix etc too

--- config/home/default.nix
+++ config/home/default.nix
@@ -1,7 +1,21 @@
-{ pkgs, lib, ... }:
-
-with import ../../lib;
+{ pkgs, lib, config, ... }:
 
 {
-  imports = recImport ./.;
+  imports = [
+    ./common.nix
+    ./desktop
+    ./direnv.nix
+    ./emacs
+    ./fish
+    ./git.nix
+    ./gnupg.nix
+    ./jetbrains.nix
+    ./latex.nix
+    ./mpv.nix
+    ./neovim
+    ./programming.nix
+    ./social.nix
+    ./terminal.nix
+    ./vscode.nix
+  ];
 }
--- config/nixos/default.nix
+++ config/nixos/default.nix
@@ -4,5 +4,18 @@ with import ../../lib;
 
 let sources = import ../../nix/sources.nix;
 in {
-  imports = [ "${sources.home-manager}/nixos" ../modules ] ++ recImport ./.;
+  imports = [
+    "${sources.home-manager}/nixos"
+    ../modules
+    ./audio.nix
+    ./boot.nix
+    ./cachix.nix
+    ./common.nix
+    ./fonts.nix
+    ./gaming.nix
+    ./programs.nix
+    ./services.nix
+    ./users.nix
+    ./virtualisation.nix
+  ];
 }
