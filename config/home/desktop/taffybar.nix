f986e9b95baade01a31d9c6722f7953743a62920
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-02-09 00:32:27 +0100

    Recompile taffybar on nixos-rebuild if changed

--- config/home/desktop/taffybar.nix
+++ config/home/desktop/taffybar.nix
@@ -11,23 +11,39 @@ in {
       default = (import ./taffybar { }).mytaffybar;
       example = pkgs.taffybar;
     };
-  };
-
-  config = mkIf cfg.enable {
-
-    systemd.user.services.taffybar = {
-      Unit = {
-        Description = "Taffybar";
-        After = [ "graphical-session-pre.target" ];
-        PartOf = [ "graphical-session.target" ];
-      };
-
-      Service = {
-        ExecStart = "${cfg.package}/bin/mytaffybar";
-        Restart = "on-failure";
-      };
-
-      Install = { WantedBy = [ "graphical-session.target" ]; };
+    config = mkOption {
+      type = types.nullOr types.path;
+      default = null;
     };
   };
+
+  config = mkIf cfg.enable (mkMerge [
+    {
+      home.packages = [ (lowPrio cfg.package) ];
+      systemd.user.services.taffybar = {
+        Unit = {
+          Description = "Taffybar";
+          After = [ "graphical-session-pre.target" ];
+          PartOf = [ "graphical-session.target" ];
+        };
+
+        Service = {
+          ExecStart = "${cfg.package}/bin/mytaffybar";
+          Restart = "on-failure";
+        };
+
+        Install = { WantedBy = [ "graphical-session.target" ]; };
+      };
+    }
+
+    (mkIf (cfg.config != null) {
+      xdg.configFile."taffybar/taffybar.hs".source = cfg.config;
+      xdg.configFile."taffybar/taffybar.hs".onChange = ''
+        echo "Recompiling Taffybar"
+
+        $DRY_RUN_CMD rm -rf $HOME/.cache/taffybar/
+        $DRY_RUN_CMD systemctl --user restart taffybar.service
+      '';
+    })
+  ]);
 }
