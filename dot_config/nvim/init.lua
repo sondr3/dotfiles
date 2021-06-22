cbbf27c581db6be09130ad626ba7088fc92cda3c
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-06-22 21:24:07 +0200

    Start configuring Neovim 0.5

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,104 @@
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  execute 'packadd packer.nvim'
+end
+
+require("packer").startup(function()
+  use 'wbthomason/packer.nvim'
+
+  use {
+    'nvim-telescope/telescope.nvim',
+    requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
+  }
+
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate"
+  }
+
+  use "neovim/nvim-lspconfig"
+  use "hrsh7th/nvim-compe"
+  use "simrat39/rust-tools.nvim"
+end)
+
+vim.opt.completeopt = { "menuone", "noselect" }
+
+require("nvim-treesitter.configs").setup {
+  ensure_installed = "maintained",
+  highlight = { enable = true }
+}
+
+vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent=true})
+vim.g.mapleader = [[ ]]
+vim.g.maplocalleader = [[,]]
+
+local nvim_lsp = require("lspconfig")
+local on_attach = function(_client, bufnr)
+  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+end
+
+local lsp_servers = { "rust_analyzer" }
+for _, lsp in ipairs(lsp_servers) do
+  nvim_lsp[lsp].setup { on_attach = on_attach }
+end
+
+require("lspconfig").rust_analyzer.setup {}
+require("rust-tools").setup {}
+
+require("compe").setup {
+  enabled = true;
+  autocomplete = true;
+
+  source = {
+    path = true;
+    nvim_lsp = true;
+  };
+}
+
+local t = function(str)
+  return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+local check_back_space = function()
+    local col = vim.fn.col('.') - 1
+    if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+        return true
+    else
+        return false
+    end
+end
+
+-- Use (s-)tab to:
+--- move to prev/next item in completion menuone
+--- jump to prev/next snippet's placeholder
+_G.tab_complete = function()
+  if vim.fn.pumvisible() == 1 then
+    return t "<C-n>"
+  elseif check_back_space() then
+    return t "<Tab>"
+  else
+    return vim.fn['compe#complete']()
+  end
+end
+_G.s_tab_complete = function()
+  if vim.fn.pumvisible() == 1 then
+    return t "<C-p>"
+  else
+    -- If <S-Tab> is not working in your terminal, change it to <C-h>
+    return t "<S-Tab>"
+  end
+end
+
+vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+
+vim.api.nvim_set_keymap("i", "<C-space>", "compe#complete()", { silent = true, noremap = true, expr = true })
+vim.api.nvim_set_keymap("i", "<CR>", "compe#confirm('<CR>')", { silent = true, noremap = true, expr = true })
+vim.api.nvim_set_keymap("i", "<C-e>", "compe#close('<C-e>')", { silent = true, noremap = true, expr = true })
