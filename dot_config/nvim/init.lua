a6784542d33aa3c869cf8f7b5a5823d002deab78
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-10-25 16:46:13 +0200

    Add and configure LuaSnips

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -131,20 +131,60 @@ require("nvim-treesitter.configs").setup({
 -- Y yank until the end of line  (note: this is now a default on master)
 vim.api.nvim_set_keymap("n", "Y", "y$", { noremap = true })
 
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 local cmp = require("cmp")
+local luasnip = require("luasnip")
+
 cmp.setup({
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
   mapping = {
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.close(),
     ["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, {
+      "i",
+      "s",
+    }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, {
+      "i",
+      "s",
+    }),
   },
   sources = cmp.config.sources({
-    { name = "nvim_lsp" },
-    { name = "buffer" },
-    { name = "path" },
     { name = "nvim_lua" },
+    { name = "nvim_lsp" },
+    { name = "path" },
+    { name = "luasnip" },
+    { name = "buffer", keyword_length = 5 },
   }),
 })
 
--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -70,6 +70,12 @@ require("packer").startup({
     use("williamboman/nvim-lsp-installer")
     use("hrsh7th/cmp-nvim-lsp")
 
+    -- Snippets
+    use({
+      "L3MON4D3/LuaSnip",
+      requires = { "saadparwaiz1/cmp_luasnip" },
+    })
+
     -- Autocompletion
     use({
       "hrsh7th/nvim-cmp",
@@ -80,6 +86,7 @@ require("packer").startup({
           "hrsh7th/cmp-buffer",
           "hrsh7th/cmp-path",
           "hrsh7th/cmp-nvim-lua",
+          "saadparwaiz1/cmp_luasnip",
         },
       },
     })
