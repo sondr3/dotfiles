1feb3af89292a668049f462479989b75575c22a0
Sondre Nilsen <sondre@eons.io>
2021-12-10 14:22:38 +0100

    Remove Y to copy whole line now that I'm on nvim 0.6

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -136,9 +136,6 @@ require("nvim-treesitter.configs").setup({
   autotag = { enable = true },
 })
 
--- Y yank until the end of line  (note: this is now a default on master)
-vim.api.nvim_set_keymap("n", "Y", "y$", { noremap = true })
-
 local has_words_before = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
