dd5647e25181c8e425c815dad35c51b27c3c2f7f
Sondre Aasemoen <sondre@eons.io>
2022-04-14 23:43:32 +0200

    Fix new tab in wezterm not spawning at ~

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -5,4 +5,13 @@ return {
 	font = wezterm.font("PragmataPro Mono Liga"),
 	font_size = 14,
 	hide_tab_bar_if_only_one_tab = true,
+	keys = {
+		{
+			key = "t",
+			mods = "SHIFT|CTRL",
+			action = wezterm.action({
+				SpawnTab = { "CurrentPaneDomain" },
+			}),
+		},
+	},
 }
