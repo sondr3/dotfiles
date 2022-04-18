dd06398c668afaee237572112ddc0703763619f7
Sondre Aasemoen <sondre@eons.io>
2022-04-18 16:23:07 +0200

    Enable Wayland for Wezterm

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -3,6 +3,7 @@ local wezterm = require("wezterm")
 return {
 	color_scheme = "nord",
 	font = wezterm.font("PragmataPro Mono Liga"),
+	enable_wayland = true,
 	font_size = 14,
 	hide_tab_bar_if_only_one_tab = true,
 	keys = {
