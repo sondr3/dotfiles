c30c211c79e1049eff9c4c735a03bd13a774d324
Sondre Aasemoen <sondre@eons.io>
2022-08-29 15:03:27 +0200

    Move VSCode config to heime

---
+++ config/code/code.task.lua
@@ -0,0 +1,9 @@
+local hm = require("heime")
+
+return hm.task({
+  name = "vscode",
+  description = "setup and configure vscode",
+  run = function(ctx)
+    ctx:copy_file("settings.json", ctx:home_file("Library", "Application Support", "Code", "User", "settings.json"))
+  end,
+})
