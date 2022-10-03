b73a2b7db54cadb9e3d0ac39e1551ea1daa95ab5
Sondre Aasemoen <sondre@eons.io>
2022-10-03 10:18:26 +0200

    Neovim v0.8 defaults to lua file type detection

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,7 +1,3 @@
--- Use Neovim file type detection
-vim.g.do_filetype_lua = 1
-vim.g.did_load_filetypes = 0
-
 -- Speed up loading Nvim files
 local impatient_ok, _ = pcall(require, "impatient")
 if not impatient_ok then
