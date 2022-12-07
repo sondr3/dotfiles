fdd081c328f850ee419fe6dd25f3b3fc305390d3
Sondre Aasemoen <sondre@eons.io>
2022-12-07 14:46:57 +0100

    Fix font size in wezterm because my eyes are bad

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -15,7 +15,7 @@ return {
   color_scheme = "nord",
   font = wezterm.font("PragmataPro Mono Liga"),
   enable_wayland = true,
-  font_size = 14,
+  font_size = 16,
   hide_tab_bar_if_only_one_tab = true,
   keys = {
     {
