4d9656de7d7e19469d051ac3cb5d8d57904f2dd7
Sondre Aasemoen <sondre@eons.io>
2022-12-09 13:05:39 +0100

    Configure wezterm for linux/macos

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,10 +1,22 @@
 local wezterm = require("wezterm")
 
+local is_macos = function()
+  return wezterm.target_triple == "aarch64-apple-darwin"
+end
+
 local get_key_modifier = function()
-  if wezterm.target_triple == "aarch64-apple-darwin" then
+  if is_macos() then
     return "CMD"
   else
     return "SHIFT|CTRL"
+  end
+end
+
+local get_font_size = function()
+  if is_macos() then
+    return 16
+  else
+    return 14
   end
 end
 
@@ -15,7 +27,7 @@ return {
   color_scheme = "nord",
   font = wezterm.font("PragmataPro Mono Liga"),
   enable_wayland = true,
-  font_size = 16,
+  font_size = get_font_size(),
   hide_tab_bar_if_only_one_tab = true,
   keys = {
     {
