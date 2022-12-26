e2e27f3ee830a0e869092d290dd8aa812cda6cf9
Sondre Aasemoen <sondre@eons.io>
2022-12-26 15:21:40 +0100

    Update lockfile, copy it in nvim task

--- config/nvim/nvim.task.lua
+++ config/nvim/nvim.task.lua
@@ -6,5 +6,6 @@ return hm.task({
   run = function(ctx)
     ctx:copy_directory("nvim", ctx:config_file("nvim"))
     ctx:copy_file("stylua.toml", ctx:config_file("nvim", "stylua.toml"))
+    ctx:copy_file("lazy-lock.json", ctx:config_file("nvim", "lazy-lock.json"))
   end,
 })
