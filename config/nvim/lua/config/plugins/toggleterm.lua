5251a7267a8b2360350b4e24dc98196d4d839fe5
Sondre Aasemoen <sondre@eons.io>
2022-12-26 15:23:43 +0100

    Fix toggleterm keybindings

--- config/nvim/lua/config/plugins/toggleterm.lua
+++ config/nvim/lua/config/plugins/toggleterm.lua
@@ -15,11 +15,10 @@ M.config = function()
   function _G.set_terminal_keymaps()
     local opts = { buffer = 0 }
     vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
-    vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
-    vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-    vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-    vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-    vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+    vim.keymap.set("t", "<C-w>h", [[<Cmd>wincmd h<CR>]], opts)
+    vim.keymap.set("t", "<C-w>j", [[<Cmd>wincmd j<CR>]], opts)
+    vim.keymap.set("t", "<C-w>k", [[<Cmd>wincmd k<CR>]], opts)
+    vim.keymap.set("t", "<C-w>l", [[<Cmd>wincmd l<CR>]], opts)
   end
 
   -- Set window movement in terminal
