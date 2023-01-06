92eb3045f628694c0526f4eba42a8e41721731ea
Sondre Aasemoen <sondre@eons.io>
2023-01-06 21:49:11 +0100

    Add utilities for frecency sorting by default in telescope

--- config/nvim/lua/config/plugins/telescope.lua
+++ config/nvim/lua/config/plugins/telescope.lua
@@ -47,10 +47,12 @@ M.config = function()
   local telescope = require("telescope")
   local actions = require("telescope.actions")
   local trouble = require("trouble.providers.telescope")
+  local frecency = require("utils.frecency")
 
   telescope.setup({
     defaults = {
       initial_mode = "insert",
+      file_sorter = frecency.frecency_sorter,
       mappings = {
         i = {
           ["<esc>"] = actions.close, -- escape closes popup
---
+++ config/nvim/lua/utils/frecency.lua
@@ -0,0 +1,44 @@
+local ext = require("telescope._extensions")
+local builtins = require("telescope.builtin")
+local frecency_db = require("telescope._extensions.frecency.db_client")
+
+local fzf = ext.manager.fzf
+
+local function frecency_score(self, prompt, line, entry)
+  if prompt == nil or prompt == "" then
+    for _, file_entry in ipairs(self.state.frecency) do
+      local filepath = entry.cwd .. "/" .. entry.value
+      if file_entry.filename == filepath then
+        return 9999 - file_entry.score
+      end
+    end
+
+    return 9999
+  end
+
+  return self.default_scoring_function(self, prompt, line, entry)
+end
+
+local function frecency_start(self, prompt)
+  self.default_start(self, prompt)
+
+  if not self.state.frecency then
+    self.state.frecency = frecency_db.get_file_scores()
+  end
+end
+
+local frecency_sorter = function(opts)
+  local fzf_sorter = fzf.native_fzf_sorter()
+
+  fzf_sorter.default_scoring_function = fzf_sorter.scoring_function
+  fzf_sorter.default_start = fzf_sorter.start
+
+  fzf_sorter.scoring_function = frecency_score
+  fzf_sorter.start = frecency_start
+
+  return fzf_sorter
+end
+
+return {
+  frecency_sorter = frecency_sorter,
+}
