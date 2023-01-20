4b3fba46b11bb11b31d3f2dcd2a0d313329e1d03
Sondre Aasemoen <sondre@eons.io>
2023-01-20 14:29:53 +0100

    Update VSCode config for Linux

--- config/code/code.task.lua
+++ config/code/code.task.lua
@@ -1,12 +1,17 @@
 local hm = require("heime")
 
+local path = function(ctx)
+  if ctx.is_linux() then
+    return ctx:config_file("Code", "User", "settings.json")
+  elseif ctx.is_macos() then
+    return ctx:home_file("Library", "Application Support", "Code", "User", "settings.json")
+  end
+end
+
 return hm.task({
   name = "vscode",
   description = "setup and configure vscode",
   run = function(ctx)
-    ctx:copy_file(
-      "settings.json",
-      ctx:home_file("Library", "Application Support", "Code", "User", "settings.json")
-    )
+    ctx:copy_file("settings.json", path(ctx))
   end,
 })
