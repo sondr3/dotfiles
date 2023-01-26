0c1e306da019e91c58f27e299d48dea9c80d17bc
Sondre Aasemoen <sondre@eons.io>
2023-01-26 22:46:37 +0100

    Don't start TS or Deno LSP on directories with .git

--- config/nvim/lua/config/plugins/lsp/init.lua
+++ config/nvim/lua/config/plugins/lsp/init.lua
@@ -189,7 +189,7 @@ M.config = function()
   lspconfig.tsserver.setup({
     on_attach = M.on_attach,
     capabilities = capabilities,
-    root_dir = util.root_pattern("package.json", "tsconfig.json", ".git"),
+    root_dir = util.root_pattern("package.json", "tsconfig.json"),
     init_options = {
       lint = true,
     },
@@ -197,7 +197,7 @@ M.config = function()
   lspconfig.denols.setup({
     on_attach = M.on_attach,
     capabilities = capabilities,
-    root_dir = util.root_pattern("deno.json", "deno.jsonc", "deno.lock", ".git"),
+    root_dir = util.root_pattern("deno.json", "deno.jsonc", "deno.lock"),
     single_file_support = false,
     init_options = {
       enable = true,
