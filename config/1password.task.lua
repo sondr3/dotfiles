7e49441a65f6c73c179097830f53b5e64a8e290e
Sondre Aasemoen <sondre@eons.io>
2024-06-03 10:17:13 +0200

    Configure 1Password SSH agent

---
+++ config/1password.task.lua
@@ -0,0 +1,15 @@
+local hm = require("heime")
+
+local agent = [=[
+# Allow all files from my account
+[[ssh-keys]]
+account = "my.1password.com"
+]=]
+
+return hm.task({
+  name = "1password",
+  description = "setup and configure 1Password",
+  run = function(ctx)
+    ctx:write_string(ctx:config_file("1Password", "ssh", "agent.toml"), agent)
+  end,
+})
