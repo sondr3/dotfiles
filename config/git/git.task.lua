3dcc87334b4af3c01729285399b67037d7360a05
Sondre Aasemoen <sondre.aasemoen@aritma.com>
2024-08-30 12:46:27 +0200

    Add packages for git, use rust-parallel over xargs in git aliases

--- config/git/git.task.lua
+++ config/git/git.task.lua
@@ -4,6 +4,14 @@ return hm.task({
   name = "git",
   description = "setup and configure node",
   run = function(ctx)
+    -- ctx:install({
+    --   ["rust-parallel"] = {
+    --     windows = "cargo",
+    --     macos = "homebrew"
+    --   };
+    --   "git";
+    --   "fzf";
+    -- })
     ctx:template("gitconfig.tmpl", hm.path(hm.home_dir, ".gitconfig"))
 
     ctx:copy("gitignore-global", hm.path(hm.home_dir, ".gitignore"))
