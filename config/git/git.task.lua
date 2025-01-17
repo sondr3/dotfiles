038696629ba64445ba4a534a21277633d99153f7
Sondre Aasemoen <sondre@eons.io>
2025-01-17 17:35:10 +0100

    Use key-value arrays of tables due to pairs being unordered

--- config/git/git.task.lua
+++ config/git/git.task.lua
@@ -10,102 +10,114 @@ end
 
 local config = function(data)
   return {
-    user = {
-      name = "Sondre Aasemoen",
-      email = data:get("email"),
-      signingkey = data:get("signingkey"),
+    {
+      "user",
+      {
+        name = "Sondre Aasemoen",
+        email = data:get("email"),
+        signingkey = data:get("signingkey"),
+      },
     },
-    push = {
+    { "push", {
       default = "current",
       followTags = true,
-    },
-    pull = {
+    } },
+    { "pull", {
       default = "current",
       rebase = true,
-    },
-    rebase = {
+    } },
+    { "rebase", {
       autosquash = true,
-    },
-    commit = {
+    } },
+    { "commit", {
       gpgsign = true,
-    },
-    gpg = {
+    } },
+    { "gpg", {
       format = "ssh",
-    },
-    [ [[gpg "ssh"]] ] = {
+    } },
+    { [[gpg "ssh"]], {
       program = ssh(data),
-    },
-    advice = {
+    } },
+    { "advice", {
       addEmptyPathspec = false,
-    },
-    diff = {
+    } },
+    { "diff", {
       colorMoved = "default",
+    } },
+    {
+      "alias",
+      {
+        b = "branch -v",
+        d = "diff",
+        dc = "diff --cached",
+        cm = "commit -m",
+        can = "commit --amend --no-edit",
+        lg = "log --graph --abbrev-commit --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'",
+        r = "remote -v",
+        s = "status",
+        st = "status -bs",
+        co = "!git for-each-ref --format='%(refname:short)' refs/heads | fzf | xargs git checkout",
+        last = "log -1 HEAD --stat",
+        pp = "!git pull && git push",
+        pt = "!git pull && git pull --tags",
+        up = "!git push && git push --tags",
+        dead = "!git fetch --prune && git branch --merged | rg -v 'main|master' | xargs git branch -d",
+        gone = "!git fetch --all --prune && git branch -vv | awk '/: gone]/{print $1}' | xargs git branch -D;",
+      },
     },
-    alias = {
-      b = "branch -v",
-      d = "diff",
-      dc = "diff --cached",
-      cm = "commit -m",
-      can = "commit --amend --no-edit",
-      lg = "log --graph --abbrev-commit --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'",
-      r = "remote -v",
-      s = "status",
-      st = "status -bs",
-      co = "!git for-each-ref --format='%(refname:short)' refs/heads | fzf | xargs git checkout",
-      last = "log -1 HEAD --stat",
-      pp = "!git pull && git push",
-      pt = "!git pull && git pull --tags",
-      up = "!git push && git push --tags",
-      dead = "!git fetch --prune && git branch --merged | rg -v 'main|master' | xargs git branch -d",
-      gone = "!git fetch --all --prune && git branch -vv | awk '/: gone]/{print $1}' | xargs git branch -D;",
-    },
-    github = {
+    { "github", {
       user = "sondr3",
-    },
-    core = {
+    } },
+    { "core", {
       autocrlf = "input",
       excludesFile = "~/.gitignore",
-    },
-    init = {
+    } },
+    { "init", {
       defaultBranch = "main",
-    },
-    pager = {
+    } },
+    { "pager", {
       diff = "delta",
       log = "delta",
       reflog = "delta",
       show = "delta",
-    },
-    interactive = {
+    } },
+    { "interactive", {
       diffFilter = "delta --color-only",
+    } },
+    {
+      "delta",
+      {
+        features = "side-by-side line-numbers decorations",
+        ["whitespace-error-style"] = "22 reverse",
+      },
     },
-    delta = {
-      features = "side-by-side line-numbers decorations",
-      ["whitespace-error-style"] = "22 reverse",
-    },
-    credentials = {
+    { "credentials", {
       helper = "cache",
-    },
-    rerere = {
+    } },
+    { "rerere", {
       enabled = true,
       autoupdate = true,
+    } },
+    {
+      [[filter "lfs"]],
+      {
+        clean = "git-lfs clean -- %f",
+        smudge = "git-lfs smudge -- %f",
+        process = "git-lfs filter-process",
+        required = true,
+      },
     },
-    [ [[filter "lfs"]] ] = {
-      clean = "git-lfs clean -- %f",
-      smudge = "git-lfs smudge -- %f",
-      process = "git-lfs filter-process",
-      required = true,
-    },
-    [ [[includeIf "gitdir:~/code/projects/aritma/"]] ] = {
+    { [[includeIf "gitdir:~/code/projects/aritma/"]], {
       path = "~/.gitconfig-aritma",
-    },
+    } },
   }
 end
 
 local aritma_config = function(data)
   return {
-    user = {
+    { "user", {
       email = data:get("work_email"),
-    },
+    } },
   }
 end
 
