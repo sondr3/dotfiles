d9ea1fd3b7a3105fc0c7bda6542667d352956ecf
Sondre Aasemoen <sondre@eons.io>
2025-07-29 12:45:10 +0200

    Add config for Azure Artifacts for npm

--- config/node.task.lua
+++ config/node.task.lua
@@ -10,6 +10,17 @@ prefix=/home/sondre/.local
 save-prefix=''
 save-exact=true
 //registry.npmjs.org/:_authToken={{ npmToken }}
+
+{% if hostname == "venus.local" %}
+; begin auth token 
+//pkgs.dev.azure.com/zdataProducts/_packaging/AritmaFeed/npm/registry/:username=zdataProducts 
+//pkgs.dev.azure.com/zdataProducts/_packaging/AritmaFeed/npm/registry/:_password={{ azureRegistryPat }}
+//pkgs.dev.azure.com/zdataProducts/_packaging/AritmaFeed/npm/registry/:email=npm requires email to be set but doesn't use the value
+//pkgs.dev.azure.com/zdataProducts/_packaging/AritmaFeed/npm/:username=zdataProducts 
+//pkgs.dev.azure.com/zdataProducts/_packaging/AritmaFeed/npm/:_password={{ azureRegistryPat }}
+//pkgs.dev.azure.com/zdataProducts/_packaging/AritmaFeed/npm/:email=npm requires email to be set but doesn't use the value
+; end auth token
+{% endif %}
 ]]
 
 return hm.task({
