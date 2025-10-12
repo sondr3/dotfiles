6073cc0074585472a7de25654ba9fbe64e1b1fd4
Sondre Aasemoen <sondre@eons.io>
2025-10-12 12:50:15 +0200

    Initial plugin setup, all manual baby

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,9 @@
-vim.pack.add({ "https://github.com/andersevenrud/nordic.nvim" })
+vim.pack.add({
+	"https://github.com/andersevenrud/nordic.nvim",
+	"https://github.com/lewis6991/gitsigns.nvim",
+})
 vim.cmd.colorscheme("nordic")
 
 require("options")
 require("keymaps")
+require("plugins")
